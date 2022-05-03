d87a8c5abccc1c5ec1fa876e5324e0b8184fc8d2
Vladimir D <vovastradamus@gmail.com>
2022-05-03 17:09:29 +0500

    alerts ouside context

--- src/dialog.contexts.js
+++ src/dialog.contexts.js
@@ -1,15 +1,20 @@
 import { createContext, createElement } from "react";
-import { noop } from "./dialog.shared";
 import { useDialogs } from "./dialog.hooks";
 
+const createAlertCall = (text) => console.warn(text);
+const callOutsideContextTemplate = (fnName) =>
+  `Try to call ${fnName} outside Provider`;
+
 export const DialogContenxt = createContext({
-  closeDialog: noop,
+  closeDialog: createAlertCall(callOutsideContextTemplate("closeDialog")),
 });
 
 export const DialogsContext = createContext({
   dialogs: [],
-  pushDialog: noop,
-  closeDialogByID: noop,
+  pushDialog: createAlertCall(callOutsideContextTemplate("pushDialog")),
+  closeDialogByID: createAlertCall(
+    callOutsideContextTemplate("closeDialogByID")
+  ),
 });
 
 export const DialogsProvider = (props) => {
